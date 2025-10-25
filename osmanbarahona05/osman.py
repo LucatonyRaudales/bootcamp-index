ab0c695947f3506666752d13dc1f994d2035625b
Osmancito1 <osma_barahonas@unicah.edu>
2025-10-25 09:45:27 -0600

    Practica: Osman Barahona

---
+++ osmanbarahona05/osman.py
@@ -0,0 +1,14 @@
+# Archivo de prueba de Python
+# Bootcamp Git & GitHub - Universidad Católica
+
+def presentar_info(nombre, grupo):
+    """
+    Esta función imprime la información del estudiante y un mensaje de éxito.
+    """
+    print("¡Hola desde Python! 👋")
+    print(f"Estudiante: {nombre}")
+    print(f"Grupo: {grupo}")
+    print("¡Práctica completada con éxito! 🚀")
+
+if __name__ == "__main__":
+    presentar_info("Osman Barahona", "#1")
